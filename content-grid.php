ac22231f57661bef313b0e2f82fe14bccb5db1e9
Yvan Volochine <yvan.volochine@gmail.com>
2015-03-02 12:43:35 +0100

    content-grid: bigger thumbnails

--- content-grid.php
+++ content-grid.php
@@ -7,7 +7,7 @@
 
 <div class="col-xs-6 col-md-4">
 	<div class="card" data-content="">
-		<?php the_post_thumbnail('thumbnail', array( 'class' => 'img-responsive' )); ?>
+		<?php the_post_thumbnail('large', array( 'class' => 'img-responsive' )); ?>
 		<div class="info">
 			<a href="<?php the_permalink(); ?>">
 				<h1 class="entry-title"><?php the_title(); ?></h1>

c3b11f19ec75fa6c8f67c7c1a1af0fa816ff7356
Yvan Volochine <yvan.volochine@gmail.com>
2015-06-14 20:41:36 +0200

    add 404

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package Mixfolio
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="full-width">
+		<div id="content" role="main">
+			<article id="post-0" class="post error404 not-found">
+				<header class="entry-header">
+					<div class="center-block" style="float: none; text-align: center;">
+						<h1 class="entry-title">This page was not found.</h1>
+					</div>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>

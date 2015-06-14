8f757d42e41a7455edc3194a46e73f2799239dd8
Yvan Volochine <yvan.volochine@gmail.com>
2015-06-14 20:04:30 +0200

    disable comments

---
+++ comments.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying Comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form. The actual display of comments is
+ * handled by a callback to mixfolio_comment() which is
+ * located in the functions.php file.
+ *
+ * @package Mixfolio
+ * @since Mixfolio 1.1
+ */
+?>

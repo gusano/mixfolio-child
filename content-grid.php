af1e71568c9d400549613937b418bf29daa03418
Yvan Volochine <yvan.volochine@gmail.com>
2015-06-14 20:47:59 +0200

    css: make cards clickable

--- content-grid.php
+++ content-grid.php
@@ -9,14 +9,14 @@
 	<div class="card" data-content="">
 		<?php the_post_thumbnail('large', array( 'class' => 'img-responsive' )); ?>
 		<div class="info">
-			<a href="<?php the_permalink(); ?>">
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<footer class="entry-meta">
-					<span class="postdate">
-						<?php echo get_the_date(); ?>
-					</span>
-				</footer>
+			<a class="card-link" href="<?php the_permalink(); ?>">
 			</a>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<footer class="entry-meta">
+				<span class="postdate">
+					<?php echo get_the_date(); ?>
+				</span>
+			</footer>
 		</div>
 	</div>
 </div>
